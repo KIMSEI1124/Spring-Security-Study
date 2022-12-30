06a0f028bbfe91259bfc91e6151e01e6e1977309
KIMSEI1124 <workju1124@gmail.com>
2022-12-30 18:16:05 +0900

    feat: Remember Me 인증

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,9 +3,3 @@
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
 7
 gradlew,5/b/5bbfa66edb4db3c7c33c5181f43510990d3307f9
-u
-Esrc/main/java/io/security/basicSecurity/BasicSecurityApplication.java,8/6/866ad07c91acdf5e33da107610f69007613ac991
-k
-;src/main/java/io/security/basicSecurity/SecurityConfig.java,a/5/a59f2adc40fc9be94a157513797eaa0ba7d66855
-o
-?src/main/java/io/security/basicSecurity/SecurityController.java,6/b/6b2563c7a613bc28ce6002afc1740c7dbc725a60
--- src/main/java/io/security/basicSecurity/SecurityConfig.java
+++ src/main/java/io/security/basicSecurity/SecurityConfig.java
@@ -1,32 +1,41 @@
 package io.security.basicSecurity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated();
+
         http
                 .formLogin()
-                .loginPage("/loginPage")
+//                .loginPage("/loginPage")
                 .defaultSuccessUrl("/")
                 .failureUrl("/login")
                 .usernameParameter("userId")
@@ -49,8 +58,37 @@ public class SecurityConfig {
                         System.out.println("exception = " + exception.getMessage());
                         response.sendRedirect("/login");
                     }
+                });
+//                .permitAll();
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(
+                        new LogoutHandler() {
+                            @Override
+                            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                                HttpSession session = request.getSession();
+                                session.invalidate();   // 세션 무효화 작업,
+                                // 여기서 하지 않으면 SecurityContextLogoutHandler.java 에서 세션 무효화 작업을 진행한다.
+                            }
+                        }
+                )
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("/login");    // 로그아웃 성공후 이동할 페이지
+                    }
                 })
-                .permitAll();
+                .deleteCookies("remember-me");  // 쿠키 삭제
+
+        http
+                .rememberMe()
+                .rememberMeParameter("remember")
+                .tokenValiditySeconds(3600) // 1시간
+                .userDetailsService(userDetailsService);
+
         return http.build();
     }
 }
