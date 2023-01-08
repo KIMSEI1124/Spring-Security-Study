0896bd5d01ef2da2e622cd7439e9e0671ed82bbb
KIMSEI1124 <workju1124@gmail.com>
2023-01-08 15:32:06 +0900

    feat: travis test

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,3 @@
 
-<
-build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
-7
-gradlew,5/b/5bbfa66edb4db3c7c33c5181f43510990d3307f9
 u
 Esrc/main/java/io/security/basicSecurity/BasicSecurityApplication.java,8/6/866ad07c91acdf5e33da107610f69007613ac991
--- src/test/java/io/security/basicSecurity/BasicSecurityApplicationTests.java
+++ src/test/java/io/security/basicSecurity/BasicSecurityApplicationTests.java
@@ -1,5 +1,6 @@
 package io.security.basicSecurity;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,8 @@ class BasicSecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
+		int number = 10;
+		Assertions.assertThat(10).isEqualTo(number);
 	}
 
 }
