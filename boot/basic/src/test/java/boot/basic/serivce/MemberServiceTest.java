9371492c1619b4b5ef90ee446877f52c9a24ea81
KIMSEI1124 <workju1124@gmail.com>
2023-01-20 13:42:03 +0900

    Spring MVC init

--- boot/basic/src/test/java/boot/basic/serivce/MemberServiceTest.java
+++ boot/basic/src/test/java/boot/basic/serivce/MemberServiceTest.java
@@ -19,7 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
-@ExtendWith(SpringExtension.class)
+//@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Transactional
 class MemberServiceTest {
