500cddb5827e7235ebf358ef1c6a4d47876a7e1a
KIMSEI1124 <workju1124@gmail.com>
2023-02-08 16:18:58 +0900

    MVC2: @ExceptionHandler Study

--- MVC/exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ MVC/exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -20,10 +21,10 @@ public class ApiExceptionController {
         if (id.equals("ex")) {
             throw new RuntimeException("잘못된 사용자");
         }
-        if(id.equals("bad")) {
+        if (id.equals("bad")) {
             throw new IllegalArgumentException("잘못된 입력 값");
         }
-        if(id.equals("user-ex")) {
+        if (id.equals("user-ex")) {
             throw new UserException("사용자 오류");
         }
         return new MemberDto(id, "hello " + id);
@@ -39,6 +40,10 @@ public class ApiExceptionController {
         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "error.bad", new IllegalArgumentException());
     }
 
+    @GetMapping("/api/default-handler-ex")
+    public String defaultException(@RequestParam Integer data) {
+        return "ok";
+    }
 
     @Data
     @AllArgsConstructor
